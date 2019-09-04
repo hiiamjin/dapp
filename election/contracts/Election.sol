493e0112ca4f88fcda6af00ede7fe3487b930960
hiiamjin <jinz5678@gmail.com>
2019-09-04 01:19:13 -0700

    finished

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -8,11 +8,19 @@ contract Election {
         uint voteCount;
     }
 
+    //store accounts that have voted
+    mapping(address => bool) public voters;
     // store candidates
     // fetch dandidates
     mapping(uint => Candidate) public candidates;
     // store condidates count
     uint public candidatesCount;
+
+    //voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
 
     constructor () public {
         addCandidate("Candidate 1");
@@ -22,6 +30,23 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // reguiore that they haven;t voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record voter has voted
+        voters[msg.sender] = true;
+        
+        // update candidate vote count
+        candidates[_candidateId].voteCount ++;
+
+        //trigger voted evnt
+        emit votedEvent(_candidateId);
     }
 
 }
--- election/src/js/app.js
+++ election/src/js/app.js
@@ -2,7 +2,7 @@ App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
-  // hasVoted: false,
+  hasVoted: false,
 
   init: function() {
     return App.initWeb3();
@@ -35,22 +35,22 @@ App = {
     });
   },
 
-  // // Listen for events emitted from the contract
-  // listenForEvents: function() {
-  //   App.contracts.Election.deployed().then(function(instance) {
-  //     // Restart Chrome if you are unable to receive this event
-  //     // This is a known issue with Metamask
-  //     // https://github.com/MetaMask/metamask-extension/issues/2393
-  //     instance.votedEvent({}, {
-  //       fromBlock: 0,
-  //       toBlock: 'latest'
-  //     }).watch(function(error, event) {
-  //       console.log("event triggered", event)
-  //       // Reload when a new vote is recorded
-  //       App.render();
-  //     });
-  //   });
-  // },
+  // Listen for events emitted from the contract
+  listenForEvents: function() {
+    App.contracts.Election.deployed().then(function(instance) {
+      // Restart Chrome if you are unable to receive this event
+      // This is a known issue with Metamask
+      // https://github.com/MetaMask/metamask-extension/issues/2393
+      instance.votedEvent({}, {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }).watch(function(error, event) {
+        console.log("event triggered", event)
+        // Reload when a new vote is recorded
+        App.render();
+      });
+    });
+  },
 
   render: function() {
     var electionInstance;
@@ -65,6 +65,7 @@ App = {
       if (err === null) {
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
+        
       }
     });
 
@@ -89,17 +90,17 @@ App = {
           var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
           candidatesResults.append(candidateTemplate);
 
-          // // Render candidate ballot option
-          // var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-          // candidatesSelect.append(candidateOption);
+          // Render candidate ballot option
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(candidateOption);
         });
       }
-    //   return electionInstance.voters(App.account);
-    // }).then(function(hasVoted) {
-    //   // Do not allow a user to vote
-    //   if(hasVoted) {
-    //     $('form').hide();
-    //   }
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted) {
+      // Do not allow a user to vote
+      if(hasVoted) {
+        $('form').hide();
+      }
       loader.hide();
       content.show();
     }).catch(function(error) {
@@ -107,18 +108,18 @@ App = {
     });
   },
 
-  // castVote: function() {
-  //   var candidateId = $('#candidatesSelect').val();
-  //   App.contracts.Election.deployed().then(function(instance) {
-  //     return instance.vote(candidateId, { from: App.account });
-  //   }).then(function(result) {
-  //     // Wait for votes to update
-  //     $("#content").hide();
-  //     $("#loader").show();
-  //   }).catch(function(err) {
-  //     console.error(err);
-  //   });
-  // }
+  castVote: function() {
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance) {
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function(result) {
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  }
 };
 
 $(function() {
